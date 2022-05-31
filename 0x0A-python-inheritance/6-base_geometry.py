e3a0aa4fa9489b01fc0e19fbb34c911e9681aba8
obieuan <obieuan@gmail.com>
2022-05-30 21:05:47 -0500

    update

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,10 +5,10 @@ module base_geometry
 
 
 class BaseGeometry:
-    """ BaseGeometry class """
-    
-    @classmethod
-    def area(self):
-        """ area method """
-        
-        raise Exception("area() is not implemented")
+	""" BaseGeometry class """
+
+	@classmethod
+	def area(self):
+		""" area method """
+
+		raise Exception("area() is not implemented")
