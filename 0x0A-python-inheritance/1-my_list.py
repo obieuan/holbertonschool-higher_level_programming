65da52563e5379185bdd0e7fc83263681802bd51
obieuan <obieuan@gmail.com>
2022-05-30 20:52:10 -0500

    update

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module MyList
+"""
+
+
+class MyList(list):
+    """method that sorts a list"""
+    pass
+
+    def print_sorted(self):
+        """sorts a list"""
+
+        print(sorted(list(self)))
