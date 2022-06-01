f509aeac920755b476cbe79e028859c62ce77ec4
obieuan <obieuan@gmail.com>
2022-06-01 17:57:20 -0500

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""Description of module."""
+"""
+add arguments to a list and save to file
+"""
 
 
-import sys
-savejson = __import__("7-save_to_json_file").save_to_json_file
-loadjson = __import__("8-load_from_json_file").load_from_json_file
+import json
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-oldlist = []
 try:
-    oldlist = loadjson("add_item.json")
-except:
-    pass
-savejson(oldlist + sys.argv[1:], "add_item.json")
-
+    python_objet = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    python_objet = []
+for x in range(1, len(argv)):
+    python_objet.append(argv[x])
+save_to_json_file(python_objet, 'add_item.json')
