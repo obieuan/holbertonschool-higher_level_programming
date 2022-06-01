17fb9c7d11c8bf1952157cb9efc89599887e05c6
obieuan <obieuan@gmail.com>
2022-06-01 17:33:04 -0500

    update

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+number_of_lines function module.
+Define number_of_lines function.
+"""
 
-def write_file(filename="", text=""):
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+
+def number_of_lines(filename=""):
+    """Returns the number of lines of a text file (UTF8).
+    filename (file): the file, must exist, must have permissions
+    """
+    with open(filename, encoding="UTF-8") as myfile:
+        return len(myfile.readlines())
